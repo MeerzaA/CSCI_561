ec8d75ec4742edc59974656a843789bc1cb6d07c
Meerza Ahmed <Ahm.Meerza@gmail.com>
2024-09-17 00:21:27 -0400

    new

---
+++ Homework/HW_01/Homework.py
@@ -0,0 +1,28 @@
+
+with open("input.txt", "r") as file1:
+    # Reading from a file
+    print(file1.read())
+
+#Part 1
+def CreateInitialPopulation(size, cities):
+    InitialPopulation = []
+
+    # your code here
+
+    return InitialPopulation
+
+#Part 2
+def CreateMatingPool(population, RankList):
+    matingPool = []
+
+    # your code here
+
+    return matingPool
+
+# Part 3
+def Crossover(Parent1, Parent2, Start_Index, End_Index):
+    child = []
+
+    # your code here
+
+    return child
